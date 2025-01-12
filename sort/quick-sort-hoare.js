850e5a9167ac529e1b8a4cacf6ac717b7e45e668
Ram Appaiah <appaiya.rama@gmail.com>
2025-01-12 16:28:46 +0530

    [SORTING] Quick sort with Hoare partitioning.

---
+++ sort/quick-sort-hoare.js
@@ -0,0 +1,45 @@
+
+/**
+ * Quick Sort on Hoare-Partitioning
+ * 
+ * @param {list_int32} arr
+ * @return {list_int32}
+ */
+function quick_sort(arr) {
+    helper(arr, 0, arr.length-1);
+    return arr;
+}
+
+function swap(arr, first, second) {
+    let temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
+}
+
+
+function helper(arr, start, end) {
+    if(start >= end) {
+        return;
+    }
+    let pivotIndex = Math.floor(Math.random() * (end - start + 1) + start);
+    swap(arr, start, pivotIndex)
+    let smaller = start + 1;
+    let bigger = end;
+    
+    while (smaller <= bigger) {
+        if(arr[smaller] < arr[start]) {
+            smaller++
+        } else if(arr[bigger] >= arr[start]) {
+            bigger--;
+        } else {
+            swap(arr, smaller, bigger)
+            smaller++;
+            bigger--;
+        }
+    }
+    swap(arr, start, bigger);
+
+
+    helper(arr, start, bigger-1); // Left Side
+    helper(arr, bigger+1, end); // Right Side
+}
