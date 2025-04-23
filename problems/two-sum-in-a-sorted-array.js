f4aa6230c53e06dd9e82584aa0c2cc9810fa4c36
Ram Appaiah <appaiya.rama@gmail.com>
2025-04-23 16:59:49 +0530

    [Practice Problem]: Updated two sum solution with sorted and non sorted array.

--- problems/two-sum-in-a-sorted-array.js
+++ problems/two-sum-in-a-sorted-array.js
@@ -6,6 +6,9 @@
  * Output: [0, 4]
  * 
  * If target value not found return [-1, -1]
+ * 
+ * TC: O(n) - Leanier time, we iterating to all the indexes atleast once on worst case secnario.
+ * SC: O(1) -- No use of auxilary space to store the array.
  */
 
 function pair_sum_sorted_array(numbers, target) {
@@ -25,5 +28,29 @@ function pair_sum_sorted_array(numbers, target) {
     }
     
     
+    return [-1, -1];
+}
+
+
+/**
+ * Two sum in non sorted array
+ * 
+ * TC: O(n) --- single pass in for loop
+ * SC: O(n) --- hash map to store all the possible values
+ * 
+ */
+
+function twoSumNonSortedArray(nums, target) {
+    const map = new Map();
+
+    for(let i = 0; i < nums.length; i++) {
+        let temp = target - nums[i];
+
+        if(map.has(temp)) {
+            return [map.get(temp), i];
+        }
+        map.set(nums[i], i);
+    }
+
     return [-1, -1];
 }
