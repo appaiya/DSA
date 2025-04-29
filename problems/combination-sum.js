e7644b54e82ff46df6e559972144bd6c2f25ad57
Ram Appaiah <appaiya.rama@gmail.com>
2025-04-29 23:02:12 +0530

    [Practice Problem] Combination Sum

--- problems/combination-sum.js
+++ problems/combination-sum.js
@@ -1,4 +1,8 @@
 /**
+ * Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+ * Only numbers 1 through 9 are used.
+ * Each number is used at most once.
+ * 
  * Input: k = 3, n = 7 
  * Output: [[1,2,4]] 
  * Explanation: 1 + 2 + 4 = 7.
@@ -6,20 +10,21 @@
  * Leetcode 216
  * 
  * 
- * @param {number} k
- * @param {number} n
+ * @param {number} k count of the numbers
+ * @param {number} n target value
  * @return {number[][]}
  */
 var combinationSum3 = function(k, n) {
 
     let result = [];
     let nums = [];
-    for(let i = 0; i < 9; i++) {
+    for(let i = 0; i < 9; i++) { // add nums from 1 to 9 as mentioned in the conditions
         nums.push(i+1);
     }
 
     function helper(sum, i, slate) {
 
+        // back tracking flow
         if(slate.length > k || sum > n) {
             return;
         }
@@ -28,10 +33,12 @@ var combinationSum3 = function(k, n) {
             return;
         }
 
+        // base case - leaf node
         if(i === nums.length) {
             return;
         }
 
+        // Recursive case - internal node
         helper(sum, i+1, slate);
         slate.push(nums[i]);
         helper(sum+nums[i], i+1, slate);
