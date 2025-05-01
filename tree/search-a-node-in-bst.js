495ad73a7aa5ed8e930d99844bee8171102e5f4e
Ram Appaiah <appaiya.rama@gmail.com>
2025-05-01 13:14:32 +0530

    [Tree] Search a node in BST

---
+++ tree/search-a-node-in-bst.js
@@ -0,0 +1,33 @@
+/*
+For reference:
+const BinaryTreeNode = class {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+};
+*/
+/**
+ * @param {BinaryTreeNode_int32} root
+ * @param {int32} value
+ * @return {bool}
+ */
+
+function search_node_in_bst(root, value) {
+
+    let curr = root;
+
+    while (curr != null) {
+        if(value === curr.value) {
+            return true;
+        } else if(value < curr.value) {
+            current = current.left;
+        } else {
+            current = current.right;
+        }
+    }
+
+    return false;
+
+}
