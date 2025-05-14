1ac8096db09b6f773aa124e9b1156a2c096ad56f
Ram Appaiah <appaiya.rama@gmail.com>
2025-05-14 11:48:34 +0530

    [Tree] BFS - Level order traversal of binary tree - Without Queue

--- tree/level-order-traversal-of-binary-tree.js
+++ tree/level-order-traversal-of-binary-tree.js
@@ -64,3 +64,38 @@ class Queue {
         return this.items.length;
     }
 }
+
+/**
+ * Optimized version - without Queue -- Leetcode 102
+ * 
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+
+    let result = [];
+    if(!root) return result;
+    const q = [root];
+    let index = 0;
+    while(index < q.length) {
+        let levelOrder = q.length - index;
+        let tempList = [];
+        for(let i = 0; i < levelOrder; i++) {
+            let item = q[index++];
+            tempList.push(item.val);
+            if(item.left) { q.push(item.left); }
+            if(item.right) { q.push(item.right); }
+        }
+        result.push(tempList);
+    }
+    
+    return result;
+};
