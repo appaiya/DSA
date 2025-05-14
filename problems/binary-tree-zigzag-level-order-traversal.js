1f8f08023e4fada7ec18ec96ccde1c951594ea35
Ram Appaiah <appaiya.rama@gmail.com>
2025-05-14 12:51:15 +0530

    [Practice Problem] binary-tree-zigzag-level-order-traversal

---
+++ problems/binary-tree-zigzag-level-order-traversal.js
@@ -0,0 +1,39 @@
+/**
+ * Leet code - 103
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: [[3],[20,9],[15,7]]
+ * 
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function(root) {
+    let result = [];
+    if(!root) return result;
+    let q = [root];
+    let index = 0;
+    let bool = true;
+
+    while(index < q.length) {
+        bool = !bool;
+        let level = q.length - index;
+        let tempList = [];
+        for(let i = 0; i < level; i++) {
+            let n = q[index++];
+            tempList.push(n.val);
+            if(n.left) q.push(n.left);
+            if(n.right) q.push(n.right);
+        }
+        bool ? result.push([...tempList.reverse()]) : result.push([...tempList]);
+    }
+
+
+    return result;
+};
